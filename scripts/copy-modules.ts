cf789ea341d7454d2662826518ba1e24eb259f19
sting <stinggyray@gmail.com>
2022-12-15 18:05:47 -0600

    update game data

--- scripts/copy-modules.ts
+++ scripts/copy-modules.ts
@@ -351,10 +351,6 @@ void (() => {
         candidates: Object.values(candidates),
       });
 
-      if (operatorName === "char_304_zebra") {
-        console.log(JSON.stringify(denormalizedModuleObject));
-      }
-
       // Quick and dirty regular expression for any Chinese characters to log
       // any missing translations.
       if (
