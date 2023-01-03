28c7aaf95c3f434d97ffa0445b88931453518529
Ian Kim <me@ian.kim>
2023-01-03 00:37:45 -0500

    Set up Astro-Svelte integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), svelte()],
   vite: {
     plugins: [vanillaExtractPlugin()],
   },
