0642ef113d7afd18645fae39b65eed6d8593e297
Ian Kim <me@ian.kim>
2023-01-08 16:14:29 -0500

    Implement more styles for OperatorInfo

--- src/components/OperatorInfo/styles.css.ts
+++ src/components/OperatorInfo/styles.css.ts
@@ -1,41 +1,106 @@
 import { style } from "@vanilla-extract/css";
-import { vars } from "../../theme.css";
+import { rgba } from "polished";
+
+import { spacing } from "../../theme-helpers";
+import { rawColors, vars } from "../../theme.css";
 
 export const root = style({
+  display: "flex",
+  flexDirection: "column",
+  padding: spacing(3),
+});
+
+export const portraitNameRarity = style({
   display: "grid",
-  gridTemplateAreas: `
-    "portrait     name         limited"
-    "class-branch class-branch trait-toggle"
-    "trait-info   trait-info   trait-info"
-  `,
   gridTemplateColumns: "72px 1fr auto",
+  columnGap: spacing(2),
+  alignItems: "center",
 });
 
 export const portrait = style({
-  gridArea: "potrait",
   width: 72,
   height: 72,
-  objectFit: "contain",
+  borderRadius: spacing(1),
+  background: `linear-gradient(to bottom, ${rawColors.neutrals.darktone}, ${rawColors.neutrals.midtoneDarker})`,
 });
 
 export const name = style({
-  gridArea: "name",
   fontFamily: vars.typography.secondaryFontFamily,
 });
 
-export const baseName = style({});
+export const baseName = style({
+  margin: 0,
+  fontSize: "40px",
+  fontWeight: "600",
+  lineHeight: "1",
+});
 
-export const alterName = style({});
+export const alterName = style({
+  margin: 0,
+  paddingTop: spacing(0.5),
+  color: vars.colors.neutrals.gray,
+  fontSize: "20px",
+  fontWeight: 400,
+  lineHeight: 1,
+});
+
+export const classBranchTraitToggle = style({
+  display: "grid",
+  gridTemplateColumns: "1fr auto",
+  alignItems: "end",
+});
+
+export const classBranchNameAndIconWrapper = style({
+  display: "flex",
+  alignItems: "center",
+  columnGap: spacing(1),
+});
+
+export const branchNameAndIconWrapper = style([
+  classBranchNameAndIconWrapper,
+  {
+    paddingLeft: spacing(1),
+  },
+]);
 
 export const classBranch = style({
-  gridArea: "class-branch",
   display: "flex",
+  alignItems: "center",
+  lineHeight: 1,
+});
+
+export const classBranchIcon = style({
+  height: 16,
+});
+
+export const spacer = style({
+  margin: spacing(0, 2),
+});
+
+export const position = style({
+  color: vars.colors.neutrals.gray,
 });
 
 export const traitToggle = style({
-  gridArea: "trait-toggle",
+  cursor: "pointer",
+  padding: spacing(1, 2),
+  background: "none",
+  fontFamily: "inherit",
+  fontSize: "inherit",
+  fontWeight: vars.typography.body1Bold.fontWeight,
+  borderRadius: 18,
+  selectors: {
+    "&[aria-pressed='false']": {
+      opacity: 0.8,
+      color: vars.colors.neutrals.white,
+      border: `1px solid ${rgba(rawColors.neutrals.white, 0.2)}`,
+    },
+    "&[aria-pressed='true']": {
+      border: "none",
+      background: vars.colors.neutrals.white,
+      color: vars.colors.neutrals.black,
+    },
+  },
 });
 
-export const traitInfo = style({
-  gridArea: "trait-info",
-});
+export const traitInfo = style({});
