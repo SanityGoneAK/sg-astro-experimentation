28ec31cec2badadb2a9bbe00c0aa69211f2464b4
Ian Kim <me@ian.kim>
2023-01-13 23:50:20 -0500

    Switch ToggleButton outline back to border
    ... so that default focus rings appear

--- src/components/ToggleButton/styles.css.ts
+++ src/components/ToggleButton/styles.css.ts
@@ -10,18 +10,19 @@ export const root = style({
   fontFamily: "inherit",
   fontSize: "inherit",
   fontWeight: vars.typography.body1Bold.fontWeight,
-  border: "none",
+  margin: "-2px", // to compensate for 1px transparent border
+  border: "1px solid transparent",
   borderRadius: 18,
   color: vars.colors.neutrals.white,
   selectors: {
     "&[aria-pressed='false']": {
       opacity: 0.8,
-      outline: `1px solid ${rgba(rawColors.neutrals.white, 0.2)}`,
+      border: `1px solid ${rgba(rawColors.neutrals.white, 0.2)}`,
     },
     "&[aria-pressed='false']:hover": {
       opacity: 1,
       background: `${rgba(rawColors.neutrals.white, 0.1)}`,
-      outline: `1px solid ${rgba(rawColors.neutrals.white, 0.5)}`,
+      border: `1px solid ${rgba(rawColors.neutrals.white, 0.5)}`,
     },
     "&[aria-pressed='true']": {
       background: vars.colors.neutrals.white,
