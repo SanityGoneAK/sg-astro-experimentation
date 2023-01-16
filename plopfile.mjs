741e8cf5e9372a7925843b9b6faf88d97c72183e
Ian Kim <me@ian.kim>
2023-01-15 19:39:10 -0500

    Add plop templates for quickly creating new components

---
+++ plopfile.mjs
@@ -0,0 +1,19 @@
+export default function (/** @type {import('plop').NodePlopAPI} */ plop) {
+  plop.setGenerator("react-component", {
+    description: "Generate a new React component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What's the name of the new component? (e.g. 'MyComponent')",
+      },
+    ],
+    actions: [
+      {
+        type: "addMany",
+        templateFiles: ["plop-templates/Component/*.hbs"],
+        destination: "src/components/{{ pascalCase name }}",
+      },
+    ],
+  });
+}
