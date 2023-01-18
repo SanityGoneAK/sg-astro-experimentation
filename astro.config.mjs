f614a34b782b535e52f760c8625aa17e9a1614c0
Ian Kim <me@ian.kim>
2023-01-17 23:36:16 -0500

    Force JSON imports into separate chunks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "astro/config";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@astrojs/react";
@@ -8,5 +9,20 @@ export default defineConfig({
   integrations: [react(), svelte()],
   vite: {
     plugins: [vanillaExtractPlugin()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            // force .json imports into their own chunks
+            // so that they aren't inadvertently redownloaded
+            if (id.endsWith(".json")) {
+              const { base } = path.parse(id);
+              return base;
+            }
+            return;
+          },
+        },
+      },
+    },
   },
 });
