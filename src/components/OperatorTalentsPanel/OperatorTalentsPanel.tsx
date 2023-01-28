629dc89001db374b6155c89224a0365e1a535f2d
sting <stinggyray@gmail.com>
2023-01-27 21:28:40 -0600

    Fix operator talent panel bug

--- src/components/OperatorTalentsPanel/OperatorTalentsPanel.tsx
+++ src/components/OperatorTalentsPanel/OperatorTalentsPanel.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useStore } from "@nanostores/react";
 
 import EliteButtonGroup from "../EliteButtonGroup";
@@ -8,30 +8,37 @@ import { operatorStore } from "../../pages/operators/_store";
 
 import * as classes from "./styles.css";
 
+const potentialsMap: { [eliteLevel: number]: number[] } = {};
+
 const OperatorTalentsPanel: React.FC = () => {
   const operator = useStore(operatorStore);
   const maxElite = operator.phases.length - 1;
   const [elite, setElite] = useState(maxElite);
+
+  // Compute the map of available potentials at each elite level.
+  // Only needs to be done once, so an empty useMemo is used.
+  useMemo(() => {
+    operator.talents.forEach((talent) => {
+      talent.candidates.forEach((talentPhase) => {
+        const { phase: eliteLevel } = talentPhase.unlockCondition;
+        const { requiredPotentialRank: pot } = talentPhase;
+
+        potentialsMap[eliteLevel] = [
+          ...new Set([pot, ...(potentialsMap[eliteLevel] ?? [])]),
+        ].sort();
+      });
+    });
+  }, []);
+
   const handleEliteChange = (newElite: number) => {
     setElite(newElite);
     setPotential(potentialsMap[maxElite][0]);
   };
 
-  const potentialsMap: { [eliteLevel: number]: number[] } = {};
-  operator.talents.forEach((talent) => {
-    talent.candidates.forEach((talentPhase) => {
-      const { phase: eliteLevel } = talentPhase.unlockCondition;
-      const { requiredPotentialRank: potential } = talentPhase;
-
-      potentialsMap[eliteLevel] = [
-        potential,
-        ...new Set(potentialsMap[eliteLevel] ?? []),
-      ].sort();
-    });
-  });
   const [potential, setPotential] = useState(potentialsMap[maxElite][0]);
 
   console.log(operator.talents);
+  console.log(potentialsMap);
   return (
     <>
       <div className={classes.knobsContainer}>
