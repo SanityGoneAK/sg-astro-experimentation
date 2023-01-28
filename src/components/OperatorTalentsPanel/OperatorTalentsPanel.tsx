d5bfa85a6af392d29f6255d1384f676c8f770296
Ian Kim <me@ian.kim>
2023-01-28 15:02:54 -0500

    Fix potentialsMap calculation

--- src/components/OperatorTalentsPanel/OperatorTalentsPanel.tsx
+++ src/components/OperatorTalentsPanel/OperatorTalentsPanel.tsx
@@ -8,28 +8,27 @@ import { operatorStore } from "../../pages/operators/_store";
 
 import * as classes from "./styles.css";
 
-const potentialsMap: { [eliteLevel: number]: number[] } = {};
-
 const OperatorTalentsPanel: React.FC = () => {
   const operator = useStore(operatorStore);
   const maxElite = operator.phases.length - 1;
   const [elite, setElite] = useState(maxElite);
-  const [potential, setPotential] = useState(potentialsMap[maxElite][0]);
 
   // Compute the map of available potentials at each elite level.
   // Only needs to be done once, so an empty useMemo is used.
-  useMemo(() => {
+  const potentialsMap = useMemo(() => {
+    const potentialsMap: { [eliteLevel: number]: number[] } = {};
     operator.talents.forEach((talent) => {
       talent.candidates.forEach((talentPhase) => {
         const { phase: eliteLevel } = talentPhase.unlockCondition;
         const { requiredPotentialRank: pot } = talentPhase;
-
         potentialsMap[eliteLevel] = [
           ...new Set([pot, ...(potentialsMap[eliteLevel] ?? [])]),
         ].sort();
       });
     });
+    return potentialsMap;
   }, []);
+  const [potential, setPotential] = useState(potentialsMap[maxElite][0]);
 
   const handleEliteChange = (newElite: number) => {
     setElite(newElite);
@@ -61,7 +60,7 @@ const OperatorTalentsPanel: React.FC = () => {
             talent={talent}
             eliteLevel={elite}
             potential={potential}
-          ></OperatorTalent>
+          />
         ))}
       </div>
     </>
