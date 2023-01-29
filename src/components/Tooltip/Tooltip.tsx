27588e6dc1af103676328e94423a151faf440e85
Ian Kim <me@ian.kim>
2023-01-29 11:48:25 -0500

    Restore Tooltip component functionality

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -7,7 +7,12 @@ import "tippy.js/dist/tippy.css";
 import type { TippyProps } from "@tippyjs/react";
 
 const Tooltip: React.FC<TippyProps> = ({ children, theme, ...rest }) => {
-  return null;
+  // attempting to SSR this causes `astro build` to explode
+  // we don't actually care about SSRing a tooltip, so just return null in this case
+  if (typeof window === "undefined") {
+    return null;
+  }
+
   return (
     // offset: 8px is the height of the tooltip arrow, then spacing(0.5) is 4px
     <Tippy
